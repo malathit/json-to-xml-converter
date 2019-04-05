e66fcd657519160cd7f96cfc025968af1d32421c
malathit90 <malu.t90@gmail.com>
2019-04-06 05:03:27 +0530

    init setup

---
+++ src/main/java/com/risksense/JsonToXmlConverterApplication.java
@@ -0,0 +1,13 @@
+package com.risksense;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonToXmlConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonToXmlConverterApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/risksense/config/SwaggerConfig.java
@@ -0,0 +1,48 @@
+package com.risksense.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.boot.info.GitProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Autowired
+    BuildProperties buildProperties;
+
+    @Autowired
+    GitProperties gitProperties;
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiEndPointsInfo());
+    }
+
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("Json to XML converter")
+                .description("Built by " + gitProperties.get("build.user.name")
+                    + " with code " + gitProperties.getShortCommitId()
+                    + " from branch " + gitProperties.getBranch())
+                .contact(new Contact("Malathi", "", "malu.t90@gmail.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version(buildProperties.getVersion())
+                .build();
+    }
+}
